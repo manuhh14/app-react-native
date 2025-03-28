e68e0ae613f4bae4138167afb02aa5c2811c49de
Manuel Hernandez <manuhh14@gmail.com>
2025-03-27 22:04:12 -0600

    platilla de proyecto version reacr 0.78

---
+++ themes/Styles.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export const Styles= StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#f5f5f5',
+      },
+      text: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#333',
+      },
+})
